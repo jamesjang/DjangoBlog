97ab9828030678767e0a3bb587f0366f477deffb
James <rezjames@gmail>
2018-06-12 12:12:22 -0400

    test

--- jamesjangblog/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-app_name = 'blog'
-urlpatterns = [
-    url(r'^$', views.post_list_view, name='post_list_view'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$', views.post_detail_view, name='post_detail_view'),
-    url(r'^$', views.archived_post, name='archived_post'),
-]
--- jamesjangblog/tutorials/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-app_name = 'tutorials'
-
-urlpatterns = [
-  url(r'^$', views.post_list_view, name='post_list_view'),
-]
